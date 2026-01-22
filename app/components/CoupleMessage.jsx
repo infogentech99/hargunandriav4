a0444b6ef23ac7b1dd6251d73cd775ddffece859
fardeenpathan <md.fardeen5236@gmail.com>
2026-01-22 17:27:17 +0530

    responsive

--- app/components/CoupleMessage.jsx
+++ app/components/CoupleMessage.jsx
@@ -39,51 +39,49 @@ export default function CoupleMessage() {
     return (
 
         <div className="bg-[url('/assets/backmid.png')] bg-cover bg-no-repeat bg-[#1BB1BB]">
-            <div className="lg:h-460 md:h-290 h-305">
+            <div className="lg:h-460 md:h-290 h-385">
                 <h1 className="lg:text-4xl text-3xl text-center text-white lg:pt-42 pt-12 font-cormorant">A message from the couple</h1>
                 <h2 className="lg:text-2xl text-[16px] text-center text-white lg:pt-6 lg:px-60 px-6 lg:mt-28 mt-12">We are both so delighted that you are able to join us in celebrating what we hope will be one of the happiest days of our lives. The affection shown to us by so many people since our roka has been incredibly moving, and has touched us both deeply.
                     We would like to take this opportunity to thank everyone most sincerely for their kindness.
                     We are looking forward to see you at the wedding.</h2>
-            <div className="mt-32 flex justify-center items-center">
-  <Swiper
-    modules={[Autoplay, Pagination]}
-    autoplay={{ delay: 3500, disableOnInteraction: false }}
-    loop
-    centeredSlides={true}
-    spaceBetween={20}
-    pagination={{ clickable: true }}
-    className="w-full py-12 overflow-visible"
-    breakpoints={{
-      0: {
-        slidesPerView: 1.25, 
-      },
-      768: {
-        slidesPerView: 2.2,  
-      },
-      1024: {
-        slidesPerView: 3.25, 
-      },
-    }}
-  >
-    {testimonial.map((item, index) => (
-      <SwiperSlide key={index} className="flex justify-center">
-        <img
-          src={item.img}
-          alt=""
-          className="w-full lg:h-125 h-120 object-cover lg:rounded-[40px] rounded-[20px]"
-        />
-      </SwiperSlide>
-    ))}
-  </Swiper>
-</div>
-
-
+                <div className="mt-32 flex justify-center items-center">
+                    <Swiper
+                        modules={[Autoplay, Pagination]}
+                        autoplay={{ delay: 3500, disableOnInteraction: false }}
+                        loop
+                        centeredSlides={true}
+                        spaceBetween={20}
+                        pagination={{ clickable: true }}
+                        className="w-full py-12 overflow-visible"
+                        breakpoints={{
+                            0: {
+                                slidesPerView: 1.25,
+                            },
+                            768: {
+                                slidesPerView: 2.2,
+                            },
+                            1024: {
+                                slidesPerView: 3.25,
+                            },
+                        }}
+                    >
+                        {testimonial.map((item, index) => (
+                            <SwiperSlide key={index} className="flex justify-center">
+                                <img
+                                    src={item.img}
+                                    alt=""
+                                    className="w-full lg:h-125 h-120 object-cover lg:rounded-[40px] rounded-[20px]"
+                                />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                </div>
                 <div className="flex flex-col-1 justify-center items-center mt-16">
                     <div className="bg-[url('/assets/seap.png')] bg-cover bg-no-repeat lg:h-150 lg:w-150 h-95 w-95 md:h-120 md:w-120">
                         <h1 className="lg:text-6xl text-3xl md:text-5xl text-center text-white lg:pt-26 md:pt-22 pt-16 font-cormorant">Looking <br /> forward to <br />see you</h1>
-                    <p className="lg:text-[22px] text-[14px] text-[#DEE6FF] mt-6 text-center font-cormorant">
-                        Click the link to RSVP
-                    </p>
+                        <p className="lg:text-[22px] text-[14px] text-[#DEE6FF] mt-6 text-center font-cormorant">
+                            Click the link to RSVP
+                        </p>
                     </div>
                 </div>
             </div>
